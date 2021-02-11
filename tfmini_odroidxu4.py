83d021fbe8e8814e912292e2d44ffacccbb47d39
Felipe Cardenas Lima Namour <felipe.cln@usp.br>
2021-02-11 16:19:31 -0300

    working

--- tfmini_odroidxu4.py
+++ tfmini_odroidxu4.py
@@ -1,123 +1,55 @@
 #!/usr/bin/env python
 import odroid_wiringpi as wpi
 import time
-import serial
-
-ser = serial.Serial()
-ser.port = "/dev/ttySAC0" 
-ser.baudrate = 115200 
-
-def getTFminiData():
-    while True:
-        #time.sleep(0.1)
-        count = ser.in_waiting
-        if count > 8:
-            recv = ser.read(9)   
-            ser.reset_input_buffer() 
-            # type(recv), 'str' in python2(recv[0] = 'Y'), 'bytes' in python3(recv[0] = 89)
-            # type(recv[0]), 'str' in python2, 'int' in python3 
-            
-            if recv[0] == 0x59 and recv[1] == 0x59:     #python3
-                distance = recv[2] + recv[3] * 256
-                strength = recv[4] + recv[5] * 256
-                print('(', distance, ',', strength, ')')
-                ser.reset_input_buffer()
-                
-            if recv[0] == 'Y' and recv[1] == 'Y':     #python2
-                lowD = int(recv[2].encode('hex'), 16)      
-                highD = int(recv[3].encode('hex'), 16)
-                lowS = int(recv[4].encode('hex'), 16)      
-                highS = int(recv[5].encode('hex'), 16)
-                distance = lowD + highD * 256
-                strength = lowS + highS * 256
-                print(distance, strength)
-            
-            # you can also distinguish python2 and python3: 
-            #import sys
-            #sys.version[0] == '2'    #True, python2
-            #sys.version[0] == '3'    #True, python3
-
-
-if __name__ == '__main__':
-    while(1):
-        try:
-            if ser.is_open == False:
-                print("Serial not oppened")
-                ser.open()
-            getTFminiData()
-        except KeyboardInterrupt:   # Ctrl+C
-            if ser != None:
-                ser.close()
-            break
-
-
-wpi.serialClose(serial)
-'''
-#!/usr/bin/env python
-import odroid_wiringpi as wpi
-import time
-
-ser = serial.Serial("UART0", 115200)
-
-def getTFminiData():
-    while True:
-        #time.sleep(0.1)
-        count = ser.in_waiting
-        if count > 8:
-            recv = ser.read(9)   
-            ser.reset_input_buffer() 
-            # type(recv), 'str' in python2(recv[0] = 'Y'), 'bytes' in python3(recv[0] = 89)
-            # type(recv[0]), 'str' in python2, 'int' in python3 
-            
-            if recv[0] == 0x59 and recv[1] == 0x59:     #python3
-                distance = recv[2] + recv[3] * 256
-                strength = recv[4] + recv[5] * 256
-                print('(', distance, ',', strength, ')')
-                ser.reset_input_buffer()
-                
-            if recv[0] == 'Y' and recv[1] == 'Y':     #python2
-                lowD = int(recv[2].encode('hex'), 16)      
-                highD = int(recv[3].encode('hex'), 16)
-                lowS = int(recv[4].encode('hex'), 16)      
-                highS = int(recv[5].encode('hex'), 16)
-                distance = lowD + highD * 256
-                strength = lowS + highS * 256
-                print(distance, strength)
-            
-            # you can also distinguish python2 and python3: 
-            #import sys
-            #sys.version[0] == '2'    #True, python2
-            #sys.version[0] == '3'    #True, python3
-
-
-if __name__ == '__main__':
-    try:
-        if ser.is_open == False:
-            ser.open()
-        getTFminiData()
-    except KeyboardInterrupt:   # Ctrl+C
-        if ser != None:
-            ser.close()
-
-'''
-
-
-'''
-#!/usr/bin/env python
-import odroid_wiringpi as wpi
-import time
  
-serial = wpi.serialOpen('/dev/ttyS0', 115200)
- 
+serial = wpi.serialOpen('/dev/ttySAC0', 115200)
+
+cont = 0
+cont2 = 0
+cont3 = 0
+distancia = -1
+strenght = -1
+bit3 = 0
+bit4 = 0
+bit5 = 0
+bit6 = 0
+
 while True:
-        input_str = raw_input('Serial Input> ')
-        wpi.serialPuts(serial, input_str)
-        time.sleep(0.1)
- 
-        output_str = 'Serial Output> '
-        while wpi.serialDataAvail(serial):
-                output_str += chr(wpi.serialGetchar(serial))
-        print output_str
- 
+	output = wpi.serialGetchar(serial)
+	cont += 1
+	if cont == 3 or cont == 4:
+		#print(output)
+		cont2 += 1
+
+		#Leitura de bits ( 3 e 4 distance, 5 e 6 strenght )
+		if cont == 3:
+			bit3 = output
+		if cont == 4:
+			bit4 = output
+
+		# Calcular distance
+		if cont2 == 2:
+			cont2 = 0
+			distancia = bit3 + ( bit4 * 256 )
+
+	if cont == 5 or cont == 6:
+		#print(output)
+		cont2 += 1
+
+		#Leitura de bits ( 5 e 6 strenght )
+		if cont == 5:
+			bit5 = output
+		if cont == 6:
+			bit6 = output
+		# Calcular distance e strenght
+		if cont2 == 2:
+			cont2 = 0
+			strenght = bit5 + ( bit6 * 256 )
+
+
+	if cont == 9:
+		cont = 0
+	print('Distance = ' + str(distancia) + ' || Strenght = ' + str(strenght))				
+ 	
 wpi.serialClose(serial)
-'''
+
